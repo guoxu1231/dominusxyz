619e2104f1cee3bec66f5f25fa36ad9fcd826354
guoxu1231 <guoxu1231@gmail.com>
2015-02-15 21:28:28 +0800

    Add wrapper for "Delete/replace multiple documents" and unit tests.

--- dominus/mongolab/mongolab_rest_wrapper.py
+++ dominus/mongolab/mongolab_rest_wrapper.py
@@ -9,6 +9,7 @@ import datetime
 class MongolabRestWrapper:
     """
     Rest API Wrapper for mongolab http://docs.mongolab.com/restapi/#create-collection
+        Caller should care about the network exception not wrapper.
     """
 
     API_KEY = "oFrccImwDd3wsKmHf4WWdkM9LVRqlRx_"
@@ -22,23 +23,24 @@ class MongolabRestWrapper:
     def insert_document(self, doc_path, doc):
         """
         Mongolab Rest API Wrapper for insert documents
-        docPath = "database.collection"
-        doc = python dictionary representation of json-document.
+            docPath = "database.collection"
+            doc = single json-document (python dict) or multiple documents(python dict list)
         sample url https://api.mongolab.com/api/1/databases/my-db/collections/my-coll?apiKey=myAPIKey
         """
         assert len(doc_path.split(".")) == 2
 
         rest_url = "https://api.mongolab.com/api/1/databases/%s/collections/%s" % tuple(doc_path.split("."))
-        # TODO network timeout
-        response = requests.post(rest_url, params=self.HTTP_API_KEY, headers=self.HTTP_HEADERS,
+        response = requests.post(rest_url, params=self.HTTP_API_KEY,
+                                 headers=self.HTTP_HEADERS,
                                  timeout=self.HTTP_TIMIOUT,
                                  data=json.dumps(doc))
-        logging.debug("(insert_document)Request URL: %s" % response.url)
-        #  return response.json()
+        # response text : single document, json document with _id, multiple document: { "n" : 2}
+        logging.debug("(insert_document)Response text: %s" % response.text)
         return True if response.status_code == 200 or response.status_code == 201 else False
 
-    '''
-    Mongolab Rest API Wrapper for List documents
+    def list_documents(self, doc_path, q=None, c=False, f=None, fo=None, s=None, sk=None, l=None):
+        """
+        Mongolab Rest API Wrapper for List documents
         http://docs.mongolab.com/restapi/#list-documents
         q=<query> - restrict results by the specified JSON query
         c=true - return the result count for this query
@@ -47,16 +49,15 @@ class MongolabRestWrapper:
         s=<sort order> - specify the order in which to sort each specified field (1- ascending; -1 - descending)
         sk=<num results to skip> - specify the number of results to skip in the result set; useful for paging
         l=<limit> - specify the limit for the number of results (default is 1000)
-    '''
+        """
 
-    def list_documents(self, doc_path, q=None, c=False, f=None, fo=None, s=None, sk=None, l=None):
         assert len(doc_path.split(".")) == 2
 
         rest_url = "https://api.mongolab.com/api/1/databases/%s/collections/%s" % tuple(doc_path.split("."))
 
         # Send the request.
         send_kwargs = {}
-        send_kwargs.update(self.HTTP_API_KEY)
+        send_kwargs.update(MongolabRestWrapper.HTTP_API_KEY)
         # Note that any dictionary key whose value is None will not be added to the URL's query string.
         send_kwargs['q'] = json.dumps(q) if q else None
         send_kwargs['c'] = json.dumps(c) if c else None
@@ -67,20 +68,71 @@ class MongolabRestWrapper:
         send_kwargs['l'] = json.dumps(l) if l else None
 
         # send_kwargs.
-        # TODO network timeout
-        response = requests.get(rest_url, params=send_kwargs, headers=self.HTTP_HEADERS, timeout=self.HTTP_TIMIOUT)
+        response = requests.get(rest_url, params=send_kwargs, headers=self.HTTP_HEADERS,
+                                timeout=self.HTTP_TIMIOUT)
         logging.debug("(list_documents)Request URL: %s" % response.url)
-        print("Request URL: %s" % response.url)
+        logging.debug("(list_documents)Response Text: %s" % response.text)
+        # print("Request URL: %s" % response.url)
         return response.json()
 
-     # def delete_document
+    def delete_documents(self, doc_path, q=None):
+        """
+        Mongolab REST API Wrapper for Delete/replace multiple documents
+            http://docs.mongolab.com/restapi/#view-edit-delete-document
+        :param doc_path:
+        :param q=<query> - only delete the document(s) matching the specified JSON query
+        :return: number of removed documents
+        """
+        assert len(doc_path.split(".")) == 2
+
+        rest_url = "https://api.mongolab.com/api/1/databases/%s/collections/%s" % tuple(doc_path.split("."))
+        # send_kwargs.
+        send_kwargs = {}
+        send_kwargs.update(MongolabRestWrapper.HTTP_API_KEY)
+        send_kwargs['q'] = json.dumps(q) if q else None
+        # send_kwargs['m'] = True
+
+        # Specifying an empty list in the body is equivalent to deleting the documents
+        response = requests.put(rest_url, params=send_kwargs, headers=self.HTTP_HEADERS,
+                                timeout=self.HTTP_TIMIOUT, data="[]")
+        logging.debug("(delete_documents)Request URL: %s" % response.url)
+        # { "n" : 0 , "removed" : 1}
+        logging.debug("(delete_documents)Response Text: %s" % response.text)
+        # print("Request URL: %s" % response.url)
+        return response.json()["removed"]
+
 
 MONGOLAB_REST_WRAPPER = MongolabRestWrapper()
 
+
 def main():
-    # helper = MongolabRestWrapper()
-    print MONGOLAB_REST_WRAPPER.insert_document("shawguo.movie_event", {"name": "shawguo1", "movie_data": "today","sysdate":{"$date":datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")}})
+    logging.basicConfig(level=logging.DEBUG)
+
+    assert MONGOLAB_REST_WRAPPER.delete_documents("shawguo.testcase", q={"test": 1234}) == 0
+
+    MONGOLAB_REST_WRAPPER.delete_documents("shawguo.testcase", q={"name": "shawguo1"})
+
+    assert MONGOLAB_REST_WRAPPER.insert_document("shawguo.testcase", {"name": "shawguo1", "is_debug": True, "id": 1234,
+                                                                      "sysdate": {
+                                                                          "$date": datetime.datetime.utcnow().strftime(
+                                                                              "%Y-%m-%dT%H:%M:%SZ")}}) == True
+
+    assert MONGOLAB_REST_WRAPPER.insert_document("shawguo.testcase", [{"name": "shawguo1", "is_debug": True, "id": 1235,
+                                                                       "sysdate": {
+                                                                           "$date": datetime.datetime.utcnow().strftime(
+                                                                               "%Y-%m-%dT%H:%M:%SZ")}},
+                                                                      {"name": "shawguo1", "is_debug": True, "id": 1236,
+                                                                       "sysdate": {
+                                                                           "$date": datetime.datetime.utcnow().strftime(
+                                                                               "%Y-%m-%dT%H:%M:%SZ")}}]) == True
+
+    assert MONGOLAB_REST_WRAPPER.list_documents("shawguo.testcase", c=True, q={"id": 1234}) == 1
+
+    assert MONGOLAB_REST_WRAPPER.delete_documents("shawguo.testcase", q={"id": 1234}) == 1
+    #
+    # MONGOLAB_REST_WRAPPER.delete_documents("shawguo.testcase", q={"name": "shawguo1"})
     # print helper.list_documents("shawguo.movie_event",q={"name":"shawguo"},f={"name":1})
+
 
 if __name__ == "__main__":
     main()
