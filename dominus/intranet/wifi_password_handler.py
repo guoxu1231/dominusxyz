d5b78eaeab9afee35253c336d0dfaf9f3d6c206d
guoxu1231 <guoxu1231@gmail.com>
2015-03-17 15:33:10 +0800

    WifiPasswordHandler.post

---
+++ dominus/intranet/wifi_password_handler.py
@@ -0,0 +1,41 @@
+__author__ = 'shawguo'
+
+import webapp2
+from google.appengine.ext import ndb
+import logging
+
+
+class WifiPasswordHandler(webapp2.RequestHandler):
+    """
+        intranet shell post wifi password and will be handled by this handler
+            post: save password and misc info;
+            get: query latest wifi password;
+    """
+
+    def get(self):
+        print ''
+
+    def post(self):
+        password = self.request.get('wifi_password')
+        remote_address = (self.request.remote_addr if self.request else '')
+        assert password != ""
+
+        ndbObject = WifiPassword()
+        ndbObject.password = password
+        ndbObject.from_ip = remote_address
+        ndbObject.put()
+
+        logging.info(ndbObject)
+        self.response.write(ndbObject)
+        self.response.status = 200
+
+
+class WifiPassword(ndb.Model):
+    password = ndb.StringProperty(required=True, indexed=False)
+    from_ip = ndb.StringProperty(indexed=False)
+    user_agent = ndb.StringProperty(indexed=False)
+    create_date = ndb.DateTimeProperty(auto_now_add=True)
+
+    @classmethod
+    def _get_kind(cls):
+        return 'wifi_password'
